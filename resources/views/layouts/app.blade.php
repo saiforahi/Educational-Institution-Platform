9ffef6ae7a0e9bf351a9fa3482b0d012238749aa
saiforahi <saiforahi@gmail.com>
2020-05-30 00:05:04 +0600

    css and js files are added, a layout called 'app' declared.

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>EnfoMe | Homepage</title>
+    <!-- Stylesheets -->
+    <link href="css\bootstrap.css" rel="stylesheet">
+    <link href="css\main.css" rel="stylesheet">
+    <link href="css\responsive.css" rel="stylesheet">
+
+    <link rel="shortcut icon" href="images\favicon.png" type="image/x-icon">
+    <link rel="icon" href="images\favicon.png" type="image/x-icon">
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&family=Titillium+Web:wght@300;400;600;700;900&display=swap" rel="stylesheet">
+
+    <!-- Responsive -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+    <!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]-->
+    <!--[if lt IE 9]><script src="js/respond.js"></script><![endif]-->
+</head>
+
+<body>
+    @yield('content')
+    <script src="js\jquery.js"></script>
+    <script src="js\popper.min.js"></script>
+    <script src="js\jquery.scrollTo.js"></script>
+    <script src="js\bootstrap.min.js"></script>
+    <script src="js\jquery.mCustomScrollbar.concat.min.js"></script>
+    <script src="js\jquery.fancybox.js"></script>
+    <script src="js\appear.js"></script>
+    <script src="js\swiper.min.js"></script>
+    <script src="js\element-in-view.js"></script>
+    <script src="js\jquery.paroller.min.js"></script>
+    <script src="js\parallax.min.js"></script>
+    <script src="js\tilt.jquery.min.js"></script>
+    <!--Master Slider-->
+    <script src="js\jquery.easing.min.js"></script>
+    <script src="js\owl.js"></script>
+    <script src="js\wow.js"></script>
+    <script src="js\jquery-ui.js"></script>
+    <script src="js\script.js"></script>
+
+</body>
+</html>
