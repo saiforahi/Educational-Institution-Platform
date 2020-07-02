bb835e96292310e042d0b4173640608ae24539b4
saiforahi <saiforahi@gmail.com>
2020-07-03 00:43:04 +0600

    correction in app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -250,10 +250,5 @@
     <script src="{{asset('js/app.js')}}" defer></script>
     <script src="{{asset('js/views.js')}}" defer></script>
     @yield('js-part')
-
-    <script src="{{asset('js/app.js')}}" defer></script>
-    <script src="{{asset('js/views.js')}}" defer></script>
-    @yield('js-part')
-
 </body>
 </html>
