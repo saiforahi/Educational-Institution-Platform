d58351c921286e5758d12deac423e416093c49f4
saiforahi <saiforahi@gmail.com>
2020-07-04 23:49:19 +0600

    News model, migration and controller has been declared.

--- app/Http/Controllers/InstituteController.php
+++ app/Http/Controllers/InstituteController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\InsNotification;
 
 class InstituteController extends Controller
 {
     public function fetch_all(){
         return Institute::all();
+    }
+
+    public function get_notification($notificationID){
+        return InsNotification::find($notificationID);
     }
 }
